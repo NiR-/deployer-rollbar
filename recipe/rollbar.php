d172ef89a9780d4b326fe7e700bc027845d2e554
Albin Kerouanton <albin.kerouanton@knplabs.com>
2016-11-17 19:40:54 +0100

    Migrate to deployer v4

--- recipe/rollbar.php
+++ recipe/rollbar.php
@@ -1,11 +1,12 @@
 <?php
 
+use function Deployer\{option, set, get, task, input, runLocally, run, writeln, isVerbose};
 use Symfony\Component\Console\Input\InputOption;
 
 option('rollbar-token', null, InputOption::VALUE_REQUIRED, 'Rollbar token to use (you can also use ROLLBAR_TOKEN env var).');
 option('rollbar-env', null, InputOption::VALUE_REQUIRED, 'Rollbar environment (you can al use ROLLBAR_ENV env var). Will default to the deployr env.');
 
-env('rollbar_token', function () {
+set('rollbar_token', function () {
     $rollbarToken = getenv('ROLLBAR_TOKEN');
 
     if ($rollbarToken === false && input()->hasOption('rollbar-token')) {
@@ -14,27 +15,27 @@ env('rollbar_token', function () {
 
     return $rollbarToken;
 });
-env('rollbar_env', function () {
+set('rollbar_env', function () {
     if (getenv('ROLLBAR_ENV') !== false) {
         return getenv('ROLLBAR_ENV');
     } elseif (input()->hasOption('rollbar-env')) {
         return input()->getOption('rollbar-env');
     }
 
-    return env('env');
+    return get('env');
 });
 
 task('rollbar:notify', function () {
-    env('rollbar_local_username', runLocally('git config user.name')->toString());
-    env('rollbar_comment', run('cd {{current}} && git log -1 --pretty=format:"%s"')->toString());
+    set('rollbar_local_username', runLocally('git config user.name')->toString());
+    set('rollbar_comment', run('cd {{deploy_path}}/current && git log -1 --pretty=format:"%s"')->toString());
 
     // @see https://rollbar.com/docs/deploys_other/
     $params = [
-        'access_token'   => env('rollbar_token'),
-        'environment'    => env('rollbar_env'),
-        'revision'       => run('cd {{current}} && git rev-parse HEAD')->toString(),
-        'local_username' => env('rollbar_local_username'),
-        'comment'        => env('rollbar_comment'),
+        'access_token'   => get('rollbar_token'),
+        'environment'    => get('rollbar_env'),
+        'revision'       => run('cd {{deploy_path}}/current && git rev-parse HEAD')->toString(),
+        'local_username' => get('rollbar_local_username'),
+        'comment'        => get('rollbar_comment'),
     ];
 
     $curl = run(sprintf('curl -s -XPOST --data "%s" https://api.rollbar.com/api/1/deploy/', http_build_query($params)));
